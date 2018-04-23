aa7792b012042c25a5e55b2428f90a41872de736
MetaDBA <38537103+MetaDBA@users.noreply.github.com>
2018-04-23 13:17:29 -0400

    added comments

--- find_users_and_kill.sql
+++ find_users_and_kill.sql
@@ -1,4 +1,10 @@
+-- Find locks in TESTM database
 SELECT request_session_id FROM sys.dm_tran_locks 
 WHERE resource_database_id = DB_ID('TESTM')
 
+-- Find locks in TESTL
+SELECT request_session_id FROM sys.dm_tran_locks 
+WHERE resource_database_id = DB_ID('TESTL')
+
+-- Syntax to kill session
 kill 55
